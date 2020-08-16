6893c1c73474e1b8132a36778c88d8b68dfc6ad3
Dmitry Davydov <haudvd@gmail.com>
2020-08-16 04:27:45 +0400

    very much it

--- frontend/index.js
+++ frontend/index.js
@@ -1,11 +1,20 @@
 import lemmatize from "wink-lemmatizer"
 import { setCORS } from "google-translate-api-browser"
-import { book } from "./book"
+import { book, pages } from "./book"
 
 // setting up cors-anywhere server address
 const translate = setCORS("http://cors-anywhere.herokuapp.com/")
 
-let curSentence = null
+let curSentenceEl = null
+let curWordEl = null
+let curPage = 1
+// localStorage
+const words = JSON.parse(localStorage.words || '{}')
+
+function handleError(err) {
+  console.error(err)
+  alert(err)
+}
 
 function partOfSpeech(attr) {
   const parts = {
@@ -55,10 +64,39 @@ function lookup(attr, word) {
     })
 }
 
-function main() {
-  // insert book
-  let text = document.querySelector('.text')
-  text.innerHTML = book
+function hide(el) {
+  el.classList.add('hidden')
+}
+
+function show(el) {
+  el.classList.remove('hidden')
+}
+
+function clearDict() {
+  if (curWordEl) curWordEl.classList.remove('active')
+  if (curSentenceEl) curSentenceEl.classList.remove('active')
+  const dictCont = document.querySelector('.dictionary-container')
+  hide(dictCont)
+}
+
+function showDict() {
+  const dictCont = document.querySelector('.dictionary-container')
+
+  const rect = curSentenceEl.getBoundingClientRect()
+  dictCont.classList.remove('-top')
+  dictCont.classList.remove('-bottom')
+  if (rect.bottom > (window.innerHeight / 2)) {
+    dictCont.classList.add('-top')
+  } else {
+    dictCont.classList.add('-bottom')
+  }
+
+  show(dictCont)
+}
+
+function renderText() {
+  const text = document.querySelector('.text')
+  text.innerHTML = book.slice(pages[curPage - 1][0], pages[curPage - 1][1])
 
   // process spans
   let elements = document.querySelectorAll('.text span:not(.sentence)')
@@ -66,15 +104,27 @@ function main() {
   let rememberBtn = document.querySelector('button.remember')
 
   let myFunction = function() {
-    curSentence = this.closest('.sentence')
+    clearDict()
+
+    curWordEl = this
+    const newSentenceEl = this.closest('.sentence')
+    if (curSentenceEl !== newSentenceEl) {
+      // прячем перевод, если другое предложение выделили.
+      const translationEl = document.querySelector('.translation')
+      translationEl.innerHTML = ''
+      hide(translationEl)
+    }
+
+    curSentenceEl = newSentenceEl
+    curWordEl.classList.add('active')
+    curSentenceEl.classList.add('active')
 
     let word = this.innerHTML
-    rememberBtn.setAttribute('data-word', word)
 
     let attribute = this.getAttribute("t")
     lookup(attribute, word)
       .then(res => {
-        console.log('HUI', res)
+        // console.log('HUI', res)
         let defs = res.map(r => {
           // Показывает примеры с синонимами по умолчанию.
           let examples = r.exp.map(e => `"${e}"`).filter(e => e.includes(word)).join(' ')
@@ -87,12 +137,23 @@ function main() {
         const lemmaWord = lemma(attribute, word) !== word ? `, ${lemma(attribute, word)}` : ''
         const header  = `<b>${word} (${partOfSpeech(attribute, word)}${lemmaWord})</b>`
         dict.innerHTML = `${header} <ol>${defs}</ol>`
-      })
+        showDict()
+      }).catch(handleError)
   }
 
   for (let i = 0; i < elements.length; i++) {
+    if (words[elements[i].innerHTML]) {
+      elements[i].classList.add('remembered')
+    }
     elements[i].addEventListener('click', myFunction, false)
   }
+
+  // to drop selection
+  document.addEventListener('click', ({ target }) => {
+    if (!target.closest('.text span, .dictionary-container')) {
+      clearDict()
+    }
+  })
 }
 
 function translation() {
@@ -100,34 +161,66 @@ function translation() {
   let el = document.querySelector('.translation')
 
   let myFunction = function() {
-    translate(curSentence.innerText, { from: "en", to: "ru" })
+    btn.classList.add('-process')
+    translate(curSentenceEl.innerText, { from: "en", to: "ru" })
       .then(res => {
         el.innerHTML = res.text
+        show(el)
+        btn.classList.remove('-process')
       })
-      .catch(err => {
-        console.error(err)
-        el.innerHTML = err
-      })
+      .catch(handleError)
   }
   btn.addEventListener('click', myFunction, false)
 }
 
-const words = {}
 function remember() {
   let btn = document.querySelector('button.remember')
   let dict = document.querySelector('.dictionary')
 
   let myFunction = function() {
-    const word = this.getAttribute('data-word')
-    words[word] = true
+    words[curWordEl.innerHTML] = true
+    curWordEl.classList.add('remembered')
 
-    dict.innerHTML = JSON.stringify(words)
+    localStorage.words = JSON.stringify(words)
+    btn.classList.add('-success')
+    setTimeout(() => btn.classList.remove('-success'), 500)
   }
   btn.addEventListener('click', myFunction, false)
 }
 
+function renderPagination() {
+  // insert book
+  const div = document.querySelector('.pagination')
+  const renderBtn = (page, text=page) => {
+    return `<a class="page-btn ${curPage === page && 'active'}" href="#page=${page}">${ text }</a>`
+  }
+  // That works only for one parameter!
+  curPage = parseInt(window.location.hash.split('=')[1]) || 1
+
+  const pageBtns = [renderBtn(1)]
+  if (curPage > 2) pageBtns.push(renderBtn(curPage - 1, '<< Prev'))
+  if (curPage > 1) pageBtns.push(renderBtn(curPage))
+  if (pages.length > curPage + 1) pageBtns.push(renderBtn(curPage + 1, 'Next >>'))
+  if (pages.length > curPage) pageBtns.push(renderBtn(pages.length))
+
+  const pagesHTML = pageBtns.join('&nbsp')
+
+  div.innerHTML = pagesHTML
+
+  const paginationBtns = document.querySelectorAll('.pagination .page-btn')
+
+  const myFunction = function() {
+    setTimeout(renderPagination, 1)
+    setTimeout(renderText, 1)
+  }
+  for (let i = 0; i < paginationBtns.length; i++) {
+    paginationBtns[i].addEventListener('click', myFunction, false)
+  }
+}
+
 document.addEventListener("DOMContentLoaded", function(event) {
-  main()
+  renderPagination()
+  renderText()
   translation()
   remember()
 })
--- python/script.py
+++ python/script.py
@@ -1,21 +1,24 @@
+import json
 import spacy
 
-book = """
-The palace still shook occasionally as the earth rumbled in memory, groaned as if it would deny what had happened. Bars of sunlight cast through rents in the walls made motes of dust glitter where they yet hung in the air. Scorch-marks marred the walls, the floors, the ceilings. Broad black smears crossed the blistered paints and gilt of once-bright murals, soot overlaying crumbling friezes of men and animals which seemed to have attempted to walk before the madness grew quiet. The dead lay everywhere, men and women and children, struck down in attempted flight by the lightnings that had flashed down every corridor, or seized by the fires that had stalked them, or sunken into stone of the palace, the stones that had flowed and sought, almost alive, before stillness came again. In odd counterpoint, colorful tapestries and paintings, masterworks all, hung undisturbed except where bulging walls had pushed them awry. Finely carved furnishings, inlaid with ivory and gold, stood untouched except where rippling floors had toppled them. The mind-twisting had struck at the core, ignoring peripheral things.
+# book = """
+# The palace still shook occasionally as the earth rumbled in memory, groaned as if it would deny what had happened. Bars of sunlight cast through rents in the walls made motes of dust glitter where they yet hung in the air. Scorch-marks marred the walls, the floors, the ceilings. Broad black smears crossed the blistered paints and gilt of once-bright murals, soot overlaying crumbling friezes of men and animals which seemed to have attempted to walk before the madness grew quiet. The dead lay everywhere, men and women and children, struck down in attempted flight by the lightnings that had flashed down every corridor, or seized by the fires that had stalked them, or sunken into stone of the palace, the stones that had flowed and sought, almost alive, before stillness came again. In odd counterpoint, colorful tapestries and paintings, masterworks all, hung undisturbed except where bulging walls had pushed them awry. Finely carved furnishings, inlaid with ivory and gold, stood untouched except where rippling floors had toppled them. The mind-twisting had struck at the core, ignoring peripheral things.
 
-Lews Therin Telamon wandered the palace, deftly keeping his balance when the earth heaved. “Ilyena! My love, where are you?” The edge of his pale gray cloak trailed through blood as he stepped across the body of a woman, her golden-haired beauty marred by the horror of her last moments, her still-open eyes frozen in disbelief. “Where are you, my wife? Where is everyone hiding?”
+# Lews Therin Telamon wandered the palace, deftly keeping his balance when the earth heaved. “Ilyena! My love, where are you?” The edge of his pale gray cloak trailed through blood as he stepped across the body of a woman, her golden-haired beauty marred by the horror of her last moments, her still-open eyes frozen in disbelief. “Where are you, my wife? Where is everyone hiding?”
 
-His eyes caught his own reflection in a mirror hanging askew from bubbled marble. His clothes had been regal once, in gray and scarlet and gold; now the finely-woven cloth, brought by merchants from across the World Sea, was torn and dirty, thick with the same dust that covered his hair and skin. For a moment he fingered the symbol on his cloak, a circle half white and half black, the colors separated by a sinuous line. It meant something, that symbol. But the embroidered circle could not hold his attention long. He gazed at his own image with as much wonder. A tall man just into his middle years, handsome once, but now with hair already more white than brown and a face lined by strain and worry, dark eyes that had seen too much. Lews Therin began to chuckle, then threw back his head; his laughter echoed down the lifeless halls.
+# His eyes caught his own reflection in a mirror hanging askew from bubbled marble. His clothes had been regal once, in gray and scarlet and gold; now the finely-woven cloth, brought by merchants from across the World Sea, was torn and dirty, thick with the same dust that covered his hair and skin. For a moment he fingered the symbol on his cloak, a circle half white and half black, the colors separated by a sinuous line. It meant something, that symbol. But the embroidered circle could not hold his attention long. He gazed at his own image with as much wonder. A tall man just into his middle years, handsome once, but now with hair already more white than brown and a face lined by strain and worry, dark eyes that had seen too much. Lews Therin began to chuckle, then threw back his head; his laughter echoed down the lifeless halls.
 
-“Ilyena, my love! Come to me, my wife. You must see this.”
-"""
+# “Ilyena, my love! Come to me, my wife. You must see this.”
+# """
 
 # book = """
 # The one. Something is none
 # """
 
+with open('file.txt', 'r') as file:
+    book = file.read()
+
 nlp = spacy.load("en_core_web_sm")
-doc = nlp(book)
 
 tags = {
     'NOUN': 'n',
@@ -24,43 +27,75 @@ tags = {
     'VERB': 'v'
 }
 
-offset = 0
+part_start_idx = 0
+book_tags_offset = 0
+pages = []
+page_length = 8000
+res_book = ""
 
-# for token in doc:
-#     tag = tags.get(token.pos_, 'x')
-#     idx = offset + token.idx
-#     end_idx = offset + token.idx + len(token.text)
-#     # print(token.text, token.pos_, token.idx, len(token.text), tags.get(token.pos_, 'x'))
-#     if (tag != 'x'):
-#         book = book[:idx] + '<span t="' + tag + '">' + book[idx:end_idx] + '</span>' + book[end_idx:]
-#         offset = offset + 19
+def transform(part):
+    global book_tags_offset
 
-# for sent in doc.sents:
-#     print(sent.start_char, ',', sent.end_char)
+    offset = 0
+    cur_page_start = 0
+
+    doc = nlp(part)
+    for sent in doc.sents:
+        sent_idx = offset + sent.start_char
+        sent_end_idx = offset + sent.end_char
+
+        text = part[sent_idx:sent_end_idx]
+        sent_offset = -sent.start_char
+
+        for token in sent:
+            tag = tags.get(token.pos_, 'x')
+            idx = sent_offset + token.idx
+            end_idx = sent_offset + token.idx + len(token.text)
+            # print(token.text, token.idx, idx, end_idx, offset, sent_idx, text)
+            if (tag != 'x'):
+                text = text[:idx] + '<span t="' + tag + '">' + text[idx:end_idx] + '</span>' + text[end_idx:]
+
+                sent_offset = sent_offset + 19 # added span tag length
+                offset = offset + 19
+
+        part = part[:sent_idx] + '<span class="sentence">' + text + '</span>' + part[sent_end_idx:]
+        offset = offset + 30 # added span tag length
+
+        # detect pages
+        end_idx = sent.end_char + offset
+        if (end_idx - cur_page_start > page_length):
+            pages.append([
+                cur_page_start + book_tags_offset + part_start_idx,
+                end_idx + book_tags_offset + part_start_idx
+            ])
+            cur_page_start = end_idx
+
+    book_tags_offset = book_tags_offset + offset
+    return part
 
 
-for sent in doc.sents:
-    sent_idx = offset + sent.start_char
-    sent_end_idx = offset + sent.end_char
+while True:
+    end_idx = book.find('\n', part_start_idx + 100000)
+    if (end_idx == -1):
+        end_idx = part_start_idx + 100000
 
-    text = book[sent_idx:sent_end_idx]
-    sent_offset = -sent.start_char
+    part = book[part_start_idx:end_idx]
+    new_part = transform(part)
+    res_book = res_book + new_part
 
-    for token in sent:
-        tag = tags.get(token.pos_, 'x')
-        idx = sent_offset + token.idx
-        end_idx = sent_offset + token.idx + len(token.text)
-        # print(token.text, token.idx, idx, end_idx, offset, sent_idx, text)
-        if (tag != 'x'):
-            text = text[:idx] + '<span t="' + tag + '">' + text[idx:end_idx] + '</span>' + text[end_idx:]
-
-            sent_offset = sent_offset + 19 # added span tag length
-            offset = offset + 19
-
-    book = book[:sent_idx] + '<span class="sentence">' + text + '</span>' + book[sent_end_idx:]
-    offset = offset + 30 # added span tag length
-
-    # print(sent.start_char, ',', sent.end_char)
+    if (end_idx > len(book)):
+        break
+    part_start_idx = end_idx
 
 
-print(book)
+end_book_idx = len(book) + book_tags_offset
+if (len(pages) == 0):
+    pages.append([0, end_book_idx])
+if (pages[-1][1] < end_book_idx):
+    pages.append([pages[-1][0] + 1, end_book_idx])
+
+js_file = "export const pages = " + json.dumps(pages) + ";\n"
+js_file = js_file + "export const book = `" + res_book + "`;\n"
+
+with open('../frontend/book.js', 'w') as file:
+    file.write(js_file)
